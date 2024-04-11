7a575edcb5ba86e655aecd4e7d4cadbd1e5766e6
infodavid00 <da40au40@gmail.com>
2024-04-11 18:14:30 +0100

    18:14

--- src/containers/Principle.jsx
+++ src/containers/Principle.jsx
@@ -1,38 +1,50 @@
 import Nav from "../component/Nav.jsx"
 import Src from "../assets/WidescreenOP.mp4"
 import "../App.css"
+import { useState } from "react";
+
+function Form() {
+  return (
+    <div> 
+       {/* <div id="nameEntry" style="display: none;">
+          <input type="text" id="fullName" placeholder="Enter your Full Name" />
+      </div>
+      <div id="emailEntry" style="display: none;">
+          <input type="text" id="email" placeholder="Enter your Email" />
+      </div>
+      <div id="infoEntry" style="display: none;">
+          <input type="text" id="info" placeholder="How did you hear about LeafTree?" />
+      </div> */}
+    </div>
+  )
+}
 
 export default function Principle() {
   const GetScreenWidth = window.innerWidth;
+  const [onForm, setonForm] = useState(false)
   return (
     <>
       <Nav />
-      <div id="openBtnCont"  style={{
+      {onForm ? <Form /> : 
+       <>
+       <div id="openBtnCont"  style={{
           bottom: GetScreenWidth >= 1260  && 
           GetScreenWidth <= 1487 === true ?
           "13%" : "8%"
         }}>
-      <button id="openBtn">
+      <button id="openBtn" onClick={()=> setonForm(true)}>
         Request Form</button>
       </div>
       <div id="HomescreeVideoContainer">
         <video id="HomescreenVideo" autoPlay muted loop>
             <source src={Src} type="video/mp4" />
         </video>
-      </div>
+      </div> 
+      </>
+    } 
     </>
   )
 }
-
-{/* <div id="nameEntry" style="display: none;">
-    <input type="text" id="fullName" placeholder="Enter your Full Name" />
-</div>
-<div id="emailEntry" style="display: none;">
-    <input type="text" id="email" placeholder="Enter your Email" />
-</div>
-<div id="infoEntry" style="display: none;">
-    <input type="text" id="info" placeholder="How did you hear about LeafTree?" />
-</div> */}
 
 
 //             <!-- <div id="emailButton">
